87a9119af3711bf3821c29055cdcba4ee528d7e9
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-09-20 19:00:22 +0700

    [FIX] api gateway lib

--- project2/gateway-service/src/main/java/com/thinhlh/gateway_service/GatewayServiceApplication.java
+++ project2/gateway-service/src/main/java/com/thinhlh/gateway_service/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.thinhlh.gateway;
+package com.thinhlh.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class GatewayApplication {
+public class GatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
