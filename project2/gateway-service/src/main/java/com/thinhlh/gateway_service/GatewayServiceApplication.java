f12e89e024220170af4a22c1ef08a19f85e3e8fe
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-09-20 19:34:54 +0700

    [FIX] upgrade maven spring boot starter

--- project2/gateway-service/src/main/java/com/thinhlh/gateway_service/GatewayServiceApplication.java
+++ project2/gateway-service/src/main/java/com/thinhlh/gateway_service/GatewayServiceApplication.java
@@ -2,10 +2,10 @@ package com.thinhlh.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
